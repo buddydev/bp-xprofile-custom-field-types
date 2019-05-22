6ca96fba5468ca9050858931e8b2288b65ce8b7d
Brajesh Singh <brajesh@cosmiccoders.com>
2019-05-22 17:48:31 +0530

    Fix file/image fields asking to reupload if the are required

--- src/field-types/class-field-type-file.php
+++ src/field-types/class-field-type-file.php
@@ -39,10 +39,6 @@ class Field_Type_File extends \BP_XProfile_Field_Type {
 			unset( $raw_properties['user_id'] );
 		}
 
-		$html = $this->get_edit_field_html_elements( array_merge(
-			array( 'type' => 'file' ),
-			$raw_properties
-		) );
 
 		$value = is_user_logged_in() ? bp_get_the_profile_field_value() : '';
 		$name  = bp_get_the_profile_field_input_name();
@@ -55,6 +51,14 @@ class Field_Type_File extends \BP_XProfile_Field_Type {
 
 		$edit_value = isset( $field->data ) && ! empty( $field->data->value ) ? $field->data->value : '-';
 
+		if ( ( empty( $value ) || '-' == $value ) && xprofile_check_is_required_field( $field->id ) ) {
+			$raw_properties['required'] = true;
+		}
+
+		$html = $this->get_edit_field_html_elements( array_merge(
+			array( 'type' => 'file' ),
+			$raw_properties
+		) );
 		?>
 
         <legend id="<?php bp_the_profile_field_input_name(); ?>-1">
@@ -133,5 +137,50 @@ class Field_Type_File extends \BP_XProfile_Field_Type {
 		$new_field_value = sprintf( '<a href="%s" rel="nofollow" class="bpxcftr-file-link">%s</a>', esc_url( $new_field_value ), __( 'Download file', 'bp-xprofile-custom-field-types' ) );
 
 		return apply_filters( 'bpxcftr_file_display_data', $new_field_value, $field_id );
+	}
+
+	/**
+	 * Override parent's implementation to avoid required attribute on input elements.
+	 *
+	 * @see \BP_XProfile_Field_Type::get_edit_field_html_elements()
+	 *
+	 * Get a sanitised and escaped string of the edit field's HTML elements and attributes.
+	 *
+	 * Must be used inside the {@link bp_profile_fields()} template loop.
+	 * This method was intended to be static but couldn't be because php.net/lsb/ requires PHP >= 5.3.
+	 *
+	 * @param array $properties Optional key/value array of attributes for this edit field.
+	 * @return string
+	 */
+	protected function get_edit_field_html_elements( array $properties = array() ) {
+
+		$required = isset( $properties['required'] ) ? true : false;
+		unset( $properties['required'] );
+		$r = bp_parse_args(
+			$properties,
+			array(
+				'id'   => bp_get_the_profile_field_input_name(),
+				'name' => bp_get_the_profile_field_input_name(),
+			)
+		);
+
+		if ( $required ) {
+			$r['aria-required'] = 'true';
+
+			// Moderators can bypass field requirements.
+			if ( ! bp_current_user_can( 'bp_moderate' ) ) {
+				$r[] = 'required';
+			}
+		}
+
+		/**
+		 * Filters the edit html elements and attributes.
+		 *
+		 * @param array  $r     Array of parsed arguments.
+		 * @param string $value Class name for the current class instance.
+		 */
+		$r = (array) apply_filters( 'bp_xprofile_field_edit_html_elements', $r, get_class( $this ) );
+
+		return bp_get_form_field_attributes( sanitize_key( bp_get_the_profile_field_name() ), $r );
 	}
 }
