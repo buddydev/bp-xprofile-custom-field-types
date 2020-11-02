64a83cdfa97f768cc1c98275cc2c4160d9d5a856
Brajesh Singh <brajesh@cosmiccoders.com>
2020-11-02 23:52:43 +0530

    Fix Web field value always showing http schema due to make_clickable filter

--- src/field-types/class-field-type-web.php
+++ src/field-types/class-field-type-web.php
@@ -26,4 +26,16 @@ class Field_Type_Web extends \BP_XProfile_Field_Type_URL {
 		$this->name     = _x( 'Website (HTML5 field)', 'xprofile field type', 'bp-xprofile-custom-field-types' );
 		$this->category = _x( 'Custom Fields', 'xprofile field type category', 'bp-xprofile-custom-field-types' );
 	}
+
+	/**
+	 * Format URL values for display.
+	 *
+	 * @param string     $field_value The URL value, as saved in the database.
+	 * @param string|int $field_id    Optional. ID of the field.
+	 * @return string URL converted to a link.
+	 */
+	public static function display_filter( $field_value, $field_id = '' ) {
+		$link = strip_tags( $field_value );
+		return '<a href="' . esc_url( $field_value ) . '" rel="nofollow">' . esc_html( $link ) . '</a>';
+	}
 }
